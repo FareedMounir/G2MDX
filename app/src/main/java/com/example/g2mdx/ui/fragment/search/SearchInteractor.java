6d47e41399f89f068a0758a951cd8ef9af402d08
unifa <uni.fareed@gmail.com>
2019-05-14 21:45:29 +0200

    add client

--- app/src/main/java/com/example/g2mdx/ui/fragment/search/SearchInteractor.java
+++ app/src/main/java/com/example/g2mdx/ui/fragment/search/SearchInteractor.java
@@ -4,18 +4,27 @@ import com.example.g2mdx.data.network.ResponseHandler;
 import com.example.g2mdx.data.network.model.Country;
 import com.example.g2mdx.utilities.Constants;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
+import okhttp3.ConnectionSpec;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.TlsVersion;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class SearchInteractor {
+class SearchInteractor {
 
     void getCountries(ResponseHandler<List<Country>> responseHandler) {
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(getClient())
+                .build();
         SearchAPI api = retrofit.create(SearchAPI.class);
         api.getCountries().enqueue(new Callback<List<Country>>() {
             @Override
@@ -32,6 +41,17 @@ public class SearchInteractor {
                 responseHandler.onFailure(t.getLocalizedMessage());
             }
         });
+    }
+
+    OkHttpClient getClient() {
+        ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                .tlsVersions(TlsVersion.TLS_1_0)
+                .allEnabledCipherSuites()
+                .build();
+
+        return new OkHttpClient.Builder()
+                .connectionSpecs(Collections.singletonList(spec))
+                .build();
     }
 
 }
